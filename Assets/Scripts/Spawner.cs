3c7f85edf81a35aa6f8714c8c46c9a7f228aa23a
Gabriel-Harvey <gharve201@caledonian.ac.uk>
2021-03-31 13:29:17 +0100

    First Committ

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public Player playerTemplate;
+    public Player playerDamaged;
+    public Player playerSpine;
+    public Player playerRibs;
+    public int value = 1;
+
+    void Start()
+    {
+        //spawnPlayer();
+        if (value <= 1)
+        {
+            spawnPlayer();
+        }
+        else if  (value >= 2)
+        {
+            spawnPlayerRibs();
+        }
+    }
+
+  
+    public void spawnPlayer()
+    {
+        Player playerClone = Instantiate(playerTemplate);
+
+        playerClone.transform.position = transform.position;   
+
+        playerClone.gameObject.SetActive(true);
+    }
+
+    public void spawnPlayerDamaged()
+    {
+        Player playerClone = Instantiate(playerDamaged);
+
+        playerClone.transform.position = transform.position;
+
+        playerClone.gameObject.SetActive(true);
+    }
+
+    public void spawnPlayerSpine()
+    {
+        Player playerClone = Instantiate(playerSpine);
+
+        playerClone.gameObject.SetActive(true);
+    }
+
+    public void spawnPlayerRibs()
+    {
+        Player playerClone = Instantiate(playerRibs);
+
+        playerClone.gameObject.SetActive(true);
+    }
+
+    public void ValueIncrease()
+    {
+        value++;
+        print(value);
+    }
+
+    public void CheckValue()
+    {
+        if (value <= 1  )
+        {
+            spawnPlayerDamaged();
+        }
+        else
+        {
+            spawnPlayerSpine();
+        }
+    }
+}
---
+++ Assets/Scripts/sceenTransitions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceenTransitions : MonoBehaviour
+{
+    public UIManager manager;
+
+    public void Room()
+    {
+        manager.livesTransfer();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Room2()
+    {
+        manager.livesTransfer();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
